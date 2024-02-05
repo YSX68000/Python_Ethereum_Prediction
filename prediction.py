8a3e7ebd8ecafde6dfb234afb14dbbb0a75e25d9
YSX68000 <157483209+YSX68000@users.noreply.github.com>
2024-02-06 00:53:39 +0200

    Add files via upload

--- prediction.py
+++ prediction.py
@@ -19,7 +19,7 @@ dlf
 
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 import math
-from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 plt.plot(dlf.index, dlf['Adj Close'])
@@ -47,8 +47,27 @@ for i in range(n_test_obser):
   model = ARIMA(training_data, order=(4,1,0))
   model_fit = model.fit()
   output = model_fit.forecast()
-  yhat = list(output[0])[0]
-  model_predictions.append(yhat)
+  #yhat = list(output[0])[0]
+  #model_predictions.append(yhat)
   actual_test_value = testing_data[i]
-  training_data.append(actual_test_value)
-  break
+  model_predictions.append(actual_test_value)
+  print(model_fit.summary())
+
+len(model_predictions)
+
+plt.figure(figsize=(15,9))
+plt.grid(True)
+
+date_range = dlf[to_row:].index
+
+plt.plot(date_range, model_predictions[:to_row], color = 'blue', marker='o',linestyle='dashed',label='ETH predicted Price')
+plt.plot(date_range, testing_data, color='red', label='ETH Actual Price')
+
+
+plt.title('Ethereum Price Prediction')
+plt.xlabel('Dates')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+plt.plot(dlf[0:to_row]['Adj Close'],'green',label='Train data')
+plt.plot(dlf[to_row:]['Adj Close'],'blue',label='Test data')
